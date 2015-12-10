d8b2ccd83e7e13b621d69d9ced45ffdcbfee0052
Martin Penkov <mart91d4@edu.easj.dk>
2015-12-10 20:30:41 +0100

    login 140% operational

---
+++ project - Copy/App10/Model/Customer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using App10.ViewModel;
+
+
+    namespace App10.Model
+    {
+    public class Customer
+    {  
+        //ID = count++, Bookings
+        public string Name { get; set; }
+        public string Pass { get; set; }
+
+        public Customer(string username, string password = "")
+        {
+            Name = username;
+            Pass = password;
+        }
+
+        public bool Equals(Customer other)
+        {
+            if (other == null)
+                return false;
+
+            return this.Name == other.Name &&
+            this.Pass == other.Pass;
+        }
+        // all this shit is to override the List.Contains()
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Customer customerObj = obj as Customer;
+            if (customerObj == null)
+                return false;
+            else
+                return Equals(customerObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public static bool operator ==(Customer person1, Customer person2)
+        {
+            if (((object)person1) == null || ((object)person2) == null)
+                return Object.Equals(person1, person2);
+
+            return person1.Equals(person2);
+        }
+
+        public static bool operator !=(Customer person1, Customer person2)
+        {
+            return !(person1 == person2);
+        }
+    }
+
+    public interface IEquateable<T>
+    {
+    }
+}
