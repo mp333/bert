11c7b948e39a83894536b23f09e83c3aee29f410
Martin Penkov <mart91d4@edu.easj.dk>
2015-12-14 15:26:17 +0100

    Done

---
+++ project - Copy/App10/ViewModel/AccomodationVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App10.Common;
+using App10.Model;
+
+namespace App10.ViewModel
+{
+   public class AccomodationVM : ViewModelBase
+   {
+       private int success;
+
+       public int Success
+       {
+           get { return success;}
+           set
+           {
+               success = value;
+               OnPropertyChanged();
+           }
+       }
+        public ObservableCollection<Accomodation> Accomodations { get; set; }
+        public AccomodationVM()
+       {
+            LyonCommand = new RelayCommand(SetLyon);
+            Accomodations = new ObservableCollection<Accomodation>();
+
+
+        }
+        public RelayCommand LyonCommand { get; set; }
+
+       private bool canbook;
+
+       public bool CanBook
+       {
+           get { return canbook; }
+           set
+           {
+               canbook = value;
+               OnPropertyChanged();
+           }
+       }
+
+       public void SetLyon()
+       {
+           CurrentAccomodation = new Accomodation("Lyon",300);
+           Success = 100;
+           CanBook = true;
+       }
+    }
+}
